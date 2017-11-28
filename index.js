ee484b3a60d32f3de82064abfa261bdb15682164
lihui.xie <lihui.xie@siemens.com>
2017-11-28 13:50:27 +0800

    Initial release

---
+++ index.js
@@ -0,0 +1,12 @@
+mocha --reporter spec
+'use strict'; 
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
+
